619ff5d44f605ea9a5ccd9b798c2368de03aedba
Arvind Rajendran <arvind@trillionthoughts.in>
2022-09-26 18:12:51 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  title = "cards"
+  posts = [
+    {
+      imageUrl: "../assets/tree.jpeg",
+      title: "Neat Tree",
+      username:"nature",
+      content:"Saw this awesome tree during my hike."
+    },
+    {
+      imageUrl: "../assets/mountain.jpeg",
+      title: "Snowy Mountain",
+      username:"mountainlover",
+      content:"Here is a picture of a snowy mountain."
+    },
+    {
+      imageUrl: "../assets/biking.jpeg",
+      title: "Mountain Biking",
+      username:"biking1222",
+      content:"I did some biking today."
+    }
+  ]
 }
